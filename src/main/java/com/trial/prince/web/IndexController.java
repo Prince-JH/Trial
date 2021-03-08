cb0300401d11774a8fb0ad3c7782a8eacb25b696
Prince_JH <alahoon@naver.com>
2021-03-08 17:23:50 +0900

    study

--- src/main/java/com/trial/prince/web/IndexController.java
+++ src/main/java/com/trial/prince/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.trial.prince.web;
 
+import com.trial.prince.config.auth.dto.SessionUser;
 import com.trial.prince.service.posts.PostsService;
 import com.trial.prince.web.Dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,24 +8,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
+
     @GetMapping("/posts/save")
-    public String postsSave(){
+    public String postsSave() {
         return "posts-save";
     }
+
     @GetMapping("/posts/update/{id}")
-    public String postUpdate(@PathVariable Long id, Model model){
+    public String postUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
 
