58666782cc8d233948655d17c4a79edf4623af51
Prince_JH <alahoon@naver.com>
2021-03-09 17:26:53 +0900

    study

--- src/main/java/com/trial/prince/web/IndexController.java
+++ src/main/java/com/trial/prince/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.trial.prince.web;
 
+import com.trial.prince.config.auth.LoginUser;
 import com.trial.prince.config.auth.dto.SessionUser;
 import com.trial.prince.service.posts.PostsService;
 import com.trial.prince.web.Dto.PostsResponseDto;
@@ -18,9 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
